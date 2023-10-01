3547e4429478edbbd486100cb55120db9c2ede7b
Yaroslav <Yaroslav@DESKTOP-QAESUSL>
2023-10-01 23:13:25 +0300

    dz_from_21_09

---
+++ src/main/java/step/learning/Files/WorkingFiles.java
@@ -0,0 +1,84 @@
+package step.learning.Files;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WorkingFiles
+{
+
+    public void Run(){
+
+                String folderPath = ".";
+
+                File folder = new File(folderPath);
+
+                System.out.println("Mode    LastModified     Length    Name");
+                for (final File fileEntry : folder.listFiles()) {
+                    System.out.print(fileEntry.canExecute() ? "e" : "-");
+                    System.out.print(fileEntry.canWrite() ? "w" : "-");
+                    System.out.print(fileEntry.canRead() ? "r     " : "-     ");
+                    System.out.print(new Date(fileEntry.lastModified()) + "       ");
+                    System.out.print(fileEntry.length() + "       ");
+                    System.out.print(fileEntry.getName());
+                    System.out.println();
+                }
+    }
+    private final File _directory;
+
+    public WorkingFiles(String path)
+    {
+        this._directory = new File(path);
+    }
+
+
+    public void Run1()  // Executes all existing logic.
+    {
+        if(Availability())
+            ShowMessage(GenerateOutput());
+        else
+            ShowMessage("Sorry, I didn't find anything in your path.");
+    }
+
+    private boolean Availability()  // The method checks the availability (existence) of the directory and
+    {                              // returns true if the directory exists and is a valid directory (folder),
+                                  // otherwise it returns false.
+        if(_directory.exists() && _directory.isDirectory())
+            return true;
+        else
+            return false;
+    }
+
+    private String GenerateOutput()  // Generates formatted output of the contents of a directory and returns it as a string.
+    {
+        StringBuilder build = new StringBuilder();
+        SimpleDateFormat date_format = new SimpleDateFormat("dd.MM.yy HH:mm");
+
+        build.append("Model\tLastWriteTime\t  Lenght\tName\n");
+        build.append("-----\t-------------\t  ------\t----\n");
+
+        File[] files = _directory.listFiles();
+
+        for(File i : files)
+        {
+            String type =  i.isDirectory() ? "d-----" : "-a----";
+            String date = date_format.format(new Date(i.lastModified()));
+            String size = i.isDirectory() ? " \t" : String.valueOf(i.length());
+            String name = i.getName();
+
+            build.append(String.format("%s\t%s\t  %s\t   %s\t\n", type, date, size, name));
+        }
+        return build.toString();
+    }
+
+    private void ShowMessage(String message) { System.out.println(message); }  // Once this method is called with a specific message as an argument, it will be displayed on the console.
+}
+
+// +  +  +  +  +  Client code.  +  +  +  +  +
+//
+// public static void main( String[] args )
+// {
+//     WorkingFiles wf = new WorkingFiles("./");
+//
+//     wf.Run();
+// }
